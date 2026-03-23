62eca08a5182678d080a370763828c40a8c29ee5
Kevin Lawver <kevin@lawver.net>
2026-03-23 16:32:17 -0400

    Fixes for passkeys

--- config/initializers/webauthn.rb
+++ config/initializers/webauthn.rb
@@ -1,5 +1,9 @@
 WebAuthn.configure do |config|
-  config.allowed_origins = [ ENV.fetch("WEBAUTHN_ORIGIN", "http://localhost:3000") ]
+  app_host = ENV.fetch("APP_HOST", "localhost:3000")
+  default_origin = Rails.env.production? ? "https://#{app_host}" : "http://#{app_host}"
+  default_rp_id = app_host.split(":").first
+
+  config.allowed_origins = [ ENV.fetch("WEBAUTHN_ORIGIN", default_origin) ]
   config.rp_name = "Did"
-  config.rp_id = ENV.fetch("WEBAUTHN_RP_ID", "localhost")
+  config.rp_id = ENV.fetch("WEBAUTHN_RP_ID", default_rp_id)
 end
