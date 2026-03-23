902f6fae1e632770af860fb17804725ef108dfaf
Kevin Lawver <kevin@lawver.net>
2026-03-23 16:07:26 -0400

    Fixing more annoying things

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,5 +1,6 @@
 import { application } from "./application"
 import DueDatePickerController from "./due_date_picker_controller"
+import FlashController from "./flash_controller"
 import PasskeyController from "./passkey_controller"
 import TagSelectorController from "./tag_selector_controller"
 import TodoFormController from "./todo_form_controller"
@@ -7,6 +8,7 @@ import TodoToggleController from "./todo_toggle_controller"
 import WeekNavController from "./week_nav_controller"
 
 application.register("due-date-picker", DueDatePickerController)
+application.register("flash", FlashController)
 application.register("passkey", PasskeyController)
 application.register("tag-selector", TagSelectorController)
 application.register("todo-form", TodoFormController)
